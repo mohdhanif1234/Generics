c581fed3b7361f55c243dc5fcb53a8f1205ff0b0
mohdhanif1234 <hanif.topia@gmail.com>
2021-10-05 16:55:33 +0530

    [Hanif]:Added a program to sort the array using the concept of generics to take more than 3 parameters

---
+++ AssigmentDay13/GenericSorting.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssigmentDay13
+{
+    class GenericMaximum<T> where T:IComparable
+    {
+        public T[] value;
+        public GenericMaximum(T[] value)
+        {
+            this.value = value;
+        }
+        
+        public T[] Sort(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+        }
+        
+        public T MaxValue(params T[] values)
+        {
+            T[] sorted_values = Sort(values);
+            return sorted_values[sorted_values.Length-1];
+        }
+        
+        public T MaxMethod()
+        {
+            var max = MaxValue(this.value);
+            return max;
+        }
+
+        public void PrintMaxValue()
+        {
+            var max = MaxValue(this.value);
+            Console.WriteLine("Maximum value is " + max);
+        }
+    }
+}
